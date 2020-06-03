0be4bd796acca3ca19a9848e0f1145883be8b02e
Trevor Thomas <thomastrevor1@gmail.com>
2020-06-03 19:47:49 +0000

    added intitals written to the server

--- client.js
+++ client.js
@@ -12,6 +12,16 @@ const connect = function() {
     console.log('Server says: ', data);
   });
 
+  conn.on("connect", () => {
+    console.log('Successfully connected to game server')
+  });
+
+
+
+  conn.on('connect', () => {
+    conn.write('Name: TJT');
+  });
+
   return conn;
 }
 
