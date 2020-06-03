a52dd486e4d033ba4c0a53ec7f34ac12c9b2f589
Trevor Thomas <thomastrevor1@gmail.com>
2020-06-03 20:27:53 +0000

    created new inout file so play doesn't hold everything

--- play.js
+++ play.js
@@ -1,22 +1,11 @@
 const net = require('net');
 const { connect } = require('./client');
+const { setupInput } = require('./input');
 
-const setupInput = function() {
-  const stdin = process.stdin;
-  stdin.setRawMode(true);
-  stdin.setEncoding('utf8');
-  stdin.resume();
-
-  const handleUserInput = function(){
-    if (key === '\u0003') {
-      process.exit();
-    }
-
-  }
-  return stdin;
-}
 
 console.log('Connecting ...');
 connect();
+
+setupInput();
 
 
